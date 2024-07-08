6f2bd5758a1742bf5de1c71392b99abc07166555
Crawford Wynnes <crawfordwynnes@gmail.com>
2024-07-08 21:14:12 +0100

    removal of head html wrapper

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script'
-import Head from 'next/head'
  
 export const metadata = {
   keywords: ['Next.js', ' React', ' JavaScript', ' SEO'],
@@ -13,9 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <>
-      <head>
       <Script strategy="beforeInteractive" defer src="https://example.com/script.js"/>
-      </head>
       <html lang="en">
         <body className={inter.className}>{children}
           <p>Test</p>
