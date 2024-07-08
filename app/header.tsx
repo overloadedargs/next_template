ee5f4d6e81c5c048cc5907b34162f228ab892b33
Crawford Wynnes <crawfordwynnes@gmail.com>
2024-07-08 19:59:49 +0100

    js in header with defer

---
+++ app/header.tsx
@@ -0,0 +1,28 @@
+// import Link from "next/link";
+// import React from "react";
+
+// const header = () => {
+//   return (
+//     <div className="header">
+//       <h1>Next Starter Project</h1>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link href={"/"}>Home</Link>
+//           </li>
+//           <li>
+//             <Link href={"/about"}>About</Link>
+//           </li>
+//           <li>
+//             <Link href={"/contact"}>Contact</Link>
+//           </li>
+//           <li>
+//             <Link href={"/"}>Login</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default header;
