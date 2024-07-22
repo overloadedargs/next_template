826dde80f6271b4f1f5ffc20520e6efbd5021a1e
Crawford Wynnes <crawfordwynnes@gmail.com>
2024-07-22 15:21:08 +0100

    css grid color nth

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -2,6 +2,40 @@
 
 // `app/table/page.tsx` is the UI for the `/table URL
 export default function Page() {
+    return (
+        <>
+            <div className="gridWrapper">
+                <div>One</div>
+                <div>Two</div>
+                <div>Three</div>
+                <div>Four</div>
+                <div>Five</div>
 
+                <div>Six</div>
+                <div>Seven</div>
+                <div>Eight</div>
+                <div>Nine</div>
+                <div>Ten</div>
 
+                <div>Eleven</div>
+                <div>Twelve</div>
+                <div>Thirteen</div>
+                <div>Fourteen</div>
+                <div>Fifteen</div>
+
+                <div>Sixteen</div>
+                <div>Seventeen</div>
+                <div>Eighteen</div>
+                <div>Nineteen</div>
+                <div>Twenty</div>
+
+                <div>Twenty One</div>
+                <div>Twenty Two</div>
+                <div>Twenty Three</div>
+                <div>Twenty Four</div>
+                <div>Twenty Five</div>
+            </div>
+
+        </>
+    )
 }
